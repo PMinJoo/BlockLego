e1e5093704ef450cfd85ed8d0e13d9639c9a5d37
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-06-02 17:39:31 +0900

    활용웹 app.js 수정(초안)

--- NANUM/app.js
+++ NANUM/app.js
@@ -1,6 +1,17 @@
 var express = require('express'); //웹서버 사용
+var expressLayouts = require('express-ejs-layouts')
+var bodyParser = require('body-parser');
 var app = express();
-app.use(express.static(__dirname + '/NANUMLEGO'));
+
+app.locals.pretty = true; // html code readability
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+var router = require('./routes/index.js')(app);
+app.use('/', router);
 
 //포트 설정
 app.listen(3300, function(){
---
+++ NANUM/routes/index.js
@@ -0,0 +1,6 @@
+module.exports = function (app) {
+  var express = require('express');
+  var router = express.Router();
+
+  return router;
+};
