bb507b4b67d99e202d80dbb7067e5968c3efa3c8
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-06-02 10:54:52 +0900

    fabric sample upload (2), app_express 수정, routes 폴더 추가 및 하위 파일 생성

--- HTML/app_express.js
+++ HTML/app_express.js
@@ -8,37 +8,10 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/assets'));
 app.use(bodyParser.urlencoded({extended:true}));
 
-let randomNumber = 000000;
+//서버가 html 렌더링을 할 때, EJS 엔진을 사용하도록 설정
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
-//----이메일 보내기
-app.post("/nodemailerTest", function(req, res){
-  let email = req.body.userEmail;
-  randomNumber = Math.floor(Math.random() * (999999-111111))+111111;
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth:{
-      user: 'blocklego02pj@gmail.com',
-      pass: 'blocklego_pjNodejs0512'
-    }
-  });
-
-  let mailOptions = {
-    from: 'blocklego02pj@gmail.com',
-    to: email,
-    subject: 'BlockLego에서 발송한 확인 메일입니다.',
-    text: '홈페이지로 돌아가 인증번호를 입력해 주세요 : ' + randomNumber
-  };
-
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-      console.log(error);
-    }
-    else{
-      console.log('Email sent: ' + info.response);
-    }
-  });
-  res.redirect("userRegister.html");
-})
 
 //인증번호 확인
 app.post("/enterAuthNum", function(req, res){
@@ -62,6 +35,9 @@ app.post("/enterAuthNum", function(req, res){
   });
   //res.redirect("userRegister.html");
 })
+
+var router = require('./routes/index.js')(app);
+app.use('/', router);
 
 
 //포트 설정
