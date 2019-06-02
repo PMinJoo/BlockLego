8b66409b94762f680539ccd0ac40a4587368a1a2
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-06-02 13:41:11 +0900

    로컬

--- HTML/app_express.js
+++ HTML/app_express.js
@@ -12,6 +12,35 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+app.post("/nodemailerTest", function(req, res, next){
+    let email = req.body.userEmail;
+    randomNumber = Math.floor(Math.random() * (999999-111111))+111111;
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'blocklego02pj@gmail.com',
+        pass: 'blocklego_pjNodejs0512'
+      }
+    });
+
+    let mailOptions ={
+      from: 'blocklego02pj@gmail.com',
+      to: email,
+      subject: 'BlockLego에서 발송한 확인 메일입니다.',
+      text: '홈페이지로 돌아가 인증번호를 입력해 주세요 : ' + randomNumber
+    };
+
+    transporter.sendMail(mailOptions, function(err, info){
+      if(error){
+        console.log(error);
+      }
+      else{
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    res.redirect("userRegister.html");
+})
 
 //인증번호 확인
 app.post("/enterAuthNum", function(req, res){
@@ -35,9 +64,6 @@ app.post("/enterAuthNum", function(req, res){
   });
   //res.redirect("userRegister.html");
 })
-
-var router = require('./routes/index.js')(app);
-app.use('/', router);
 
 
 //포트 설정
