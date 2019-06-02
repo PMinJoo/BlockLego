fe6e5251a3a56b27ca8e39fa3c3e12eb64624879
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-06-02 17:04:42 +0900

    수정

--- HTML/routes/nodemailerTest.js
+++ HTML/routes/nodemailerTest.js
@@ -40,10 +40,10 @@ module.exports = function (app) {
     let authNum = req.body.authNumber;
 
     if(randomNumber == authNum){
-      res.redirect("./single-project2.html");
+      res.redirect("/single-project2.html");
     }
     else{
-      res.send('<script type="text/javascript">alert("인증번호가 올바르지 않습니다. 다시 시도해 주세요.");document.location.href="./single-project.html";</script>');
+      res.send('<script type="text/javascript">alert("인증번호가 올바르지 않습니다. 다시 시도해 주세요.");document.location.href="/single-project.html";</script>');
       //res.render('read', {title:'인증번호 조회', pass:false})
     }
 
