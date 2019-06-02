61585dd494534577e77a6fd3bb0d254a7036a7f8
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-06-02 16:55:52 +0900

    합침

--- HTML/routes/nodemailerTest.js
+++ HTML/routes/nodemailerTest.js
@@ -35,6 +35,29 @@ module.exports = function (app) {
       res.redirect("userRegister.html");
   })
 
+
+  router.post("/enterAuthNum", function(req, res){
+    let authNum = req.body.authNumber;
+
+    if(randomNumber == authNum){
+      res.redirect("single-project2.html");
+    }
+    else{
+      res.send('<script type="text/javascript">alert("인증번호가 올바르지 않습니다. 다시 시도해 주세요.");document.location.href="single-project.html";</script>');
+      //res.render('read', {title:'인증번호 조회', pass:false})
+    }
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+        console.log(error);
+      }
+      else{
+        console.log('authenticated');
+      }
+    });
+    //res.redirect("userRegister.html");
+  })
+
   //module.exports = router;
 
 
