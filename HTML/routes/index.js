a2f0454a8f2a713e5c8f9ba8ec51fe6aa89a5ff2
ozyeon <0609ojy@naver.com>
2019-06-03 14:22:05 +0900

    디비 연결 어케하지

--- HTML/routes/index.js
+++ HTML/routes/index.js
@@ -5,8 +5,8 @@ module.exports = function (app) {
   var mysql = require('mysql');
 
     var connection = mysql.createConnection({
-        host: "192.168.240.1", //서버 로컬 IP
-        port: 3303,
+        host: "13.125.249.19", //서버 로컬 IP
+        port: 53,
         user: "root", //계정 아이디
         password: "1234", //계정 비밀번호
         database: "Block" //접속할 DB
