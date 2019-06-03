6cd5bdd346b8e6a43a7f4cca8367dcaf9cdca031
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-06-03 19:56:01 +0900

    활용웹 껍데기

--- NANUM/app.js
+++ NANUM/app.js
@@ -1,7 +1,26 @@
 var express = require('express'); //웹서버 사용
-//var expressLayouts = require('express-ejs-layouts')
+var session = require('express-session')
 var bodyParser = require('body-parser');
 var app = express();
+
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host: "",
+  port: ,
+  user: "",
+  password: "",
+  database: ""
+})
+
+var createSession = function createSession(){
+  return function(req, res, next){
+    if(!req.session.login){
+      req.session.login = 'logout';
+    }
+    next();
+  };
+};
 
 app.locals.pretty = true; // html code readability
 app.set('views', './views');
@@ -9,6 +28,13 @@ app.set('view engine', 'ejs');
 //app.use(expressLayouts);
 app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+  secret: '$%$%MyKey$%$%', //암호화 키
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 1000 * 60 * 60 }, //쿠키 유지 시간 1시간
+}));
+app.use(createSession());
 
 var router = require('./routes/index.js')(app);
 app.use('/', router);
