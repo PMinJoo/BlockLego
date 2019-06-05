c77a0da40d57b125fd20fd88ea8a35063f7b9967
ozyeon <0609ojy@naver.com>
2019-06-05 11:06:16 +0900

    app.js 문제 해결

--- NANUM/app.js
+++ NANUM/app.js
@@ -6,11 +6,10 @@ var app = express();
 var mysql = require('mysql')
 
 var connection = mysql.createConnection({
-  host: "",
-  port: ,
-  user: "",
-  password: "",
-  database: ""
+    host: "localhost", //서버 로컬 IP
+    user: "root", //계정 아이디
+    password: "1234", //계정 비밀번호
+    database: "Block" //접속할 DB
 })
 
 var createSession = function createSession(){
--- NANUM/routes/index.js
+++ NANUM/routes/index.js
@@ -5,49 +5,49 @@ module.exports = function (app) {
   var JSAlert = require("js-alert");
   var url = require('url');
 
-//   var client = mysql.createConnection({
-//       host: "localhost", //서버 로컬 IP
-//       user: "root", //계정 아이디
-//       password: "1234", //계정 비밀번호
-//       database: "Block" //접속할 DB
-//   })
-//
-//   router.get('/', function(req, res){
-//     res.status(200);
-//     res.render('index', {
-// 			url: req.url,
-//       login: req.session.login,
-//       userid: req.session.userID,
-//     });
-//   })
-//
-// //login
-//   router.post('/login', function(req, res){
-//     var id = req.body.inputId;
-//     var pw = req.body.inputPassword;
-//     var queryString = 'select * from Web where dongguk_webmail=?'
-//     client.query(queryString, [id], function (error2, data) {
-//       if (error2) {
-//         console.log(error2);
-//         res.redirect('/');
-//       } else if (!data[0]) {
-//         res.send('<script type="text/javascript">alert("아이디 혹은 비밀번호를 확인해주세요.");</script>');
-//       } else {
-//         var user = data[0];
-// 				req.session.userID = id;
-// 				req.session.login = 'login';
-//         res.redirect("index-2.html");
-// 				console.log(req.session.login);
-//       }
-//     });
-//   })
-//
-//   //Logout
-//   router.get('/logout', function(req, res){
-//     req.session.login = 'logout';
-//     res.status(200);
-//     res.redirect('/index.html');
-//   })
+  var connection = mysql.createConnection({
+      host: "localhost", //서버 로컬 IP
+      user: "root", //계정 아이디
+      password: "1234", //계정 비밀번호
+      database: "Block" //접속할 DB
+  })
+
+  router.get('/', function(req, res){
+    res.status(200);
+    res.render('index', {
+			url: req.url,
+      login: req.session.login,
+      userid: req.session.userID,
+    });
+  })
+
+//login
+  router.post('/login', function(req, res){
+    var id = req.body.inputId;
+    var pw = req.body.inputPassword;
+    var queryString = 'select * from Web where dongguk_webmail=?'
+    connection.query(queryString, [id], function (error2, data) {
+      if (error2) {
+        console.log(error2);
+        res.redirect('/');
+      } else if (!data[0]) {
+        res.send('<script type="text/javascript">alert("아이디 혹은 비밀번호를 확인해주세요.");</script>');
+      } else {
+        var user = data[0];
+				req.session.userID = id;
+				req.session.login = 'login';
+        res.redirect("index-2.html");
+				console.log(req.session.login);
+      }
+    });
+  })
+
+  //Logout
+  router.get('/logout', function(req, res){
+    req.session.login = 'logout';
+    res.status(200);
+    res.redirect('/index.html');
+  })
 
   return router;
 };
