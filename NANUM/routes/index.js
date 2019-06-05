d90875df5acb91fbb3bcf337897176dc106d625a
ozyeon <0609ojy@naver.com>
2019-06-05 13:17:06 +0900

    로그인 모니터링 구현

--- NANUM/routes/index.js
+++ NANUM/routes/index.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
   var mysql = require('mysql');
   var JSAlert = require("js-alert");
   var url = require('url');
+  var dateFormat = require('dateformat');
 
   var connection = mysql.createConnection({
       host: "localhost", //서버 로컬 IP
@@ -25,6 +26,7 @@ module.exports = function (app) {
   router.post('/login', function(req, res){
     var id = req.body.inputId;
     var pw = req.body.inputPassword;
+    var day=dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
 		res.status(200);
     var queryString = 'select * from Web where dongguk_webmail=?'
     connection.query(queryString, [id], function (error2, data) {
@@ -33,7 +35,21 @@ module.exports = function (app) {
         res.redirect('/');
       } else if (!data[0]) {
         res.send('<script type="text/javascript">alert("아이디 혹은 비밀번호를 확인해주세요.");</script>');
+          var queryString = 'insert into Monitor (login_id, login_date, login_pf) values (?, ?, ?)'
+          var params = [id, day, 0];
+          connection.query(queryString, params, function (err, rows) {
+              if (err) {
+                  console.log(err);
+              }
+          });
       } else {
+        var queryString = 'insert into Monitor (login_id, login_date, login_pf) values (?, ?, ?)'
+        var params = [id, day, 1];
+        connection.query(queryString, params, function (err, rows) {
+            if (err) {
+                console.log(err);
+            }
+        });
         var user = data[0];
 				req.session.userID = id;
 				req.session.login = 'login';
