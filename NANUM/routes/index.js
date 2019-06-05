77b0b4fb4e5d2a55472ea2fe5c47b982c3371615
ozyeon <0609ojy@naver.com>
2019-06-05 11:03:51 +0900

    ??

--- NANUM/routes/index.js
+++ NANUM/routes/index.js
@@ -3,18 +3,19 @@ module.exports = function (app) {
   var router = express.Router();
   var mysql = require('mysql');
   var JSAlert = require("js-alert");
-  //var url = require('url');
+  var url = require('url');
 
-  var client = mysql.createConnection({
-      host: "localhost", //서버 로컬 IP
-      user: "root", //계정 아이디
-      password: "1234", //계정 비밀번호
-      database: "Block" //접속할 DB
+//   var client = mysql.createConnection({
+//       host: "localhost", //서버 로컬 IP
+//       user: "root", //계정 아이디
+//       password: "1234", //계정 비밀번호
+//       database: "Block" //접속할 DB
 //   })
-// 
+//
 //   router.get('/', function(req, res){
 //     res.status(200);
 //     res.render('index', {
+// 			url: req.url,
 //       login: req.session.login,
 //       userid: req.session.userID,
 //     });
