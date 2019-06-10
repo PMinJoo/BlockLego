0734262940c067dd4e6ef941c17afae4642b75be
ozyeon <0609ojy@naver.com>
2019-06-10 21:41:11 +0900

    될까

--- HTML/routes/nodemailerTest.js
+++ HTML/routes/nodemailerTest.js
@@ -103,7 +103,7 @@ module.exports = function (app) {
       }
     });
     await invoke.invoke(key, inputName, studentNumber, phone, password);
-    res.send('<script type="text/javascript">alert("회원가입이 완료되었습니다.");document.location.href="/index.html";</script>');
+    res.send('<script type="text/javascript">alert("회원가입이 완료되었습니다. 회원님의 아이디는 "+key+"입니다.");document.location.href="/index.html";</script>');
 
   })
   //module.exports = router;
