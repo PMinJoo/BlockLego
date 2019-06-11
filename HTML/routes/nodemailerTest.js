c750dde94657d6f8af39d3405321b805e7f14ceb
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-06-11 10:33:49 +0900

    ejs

--- HTML/routes/nodemailerTest.js
+++ HTML/routes/nodemailerTest.js
@@ -103,7 +103,7 @@ module.exports = function (app) {
       }
     });
     await invoke.invoke(key, inputName, studentNumber, phone, password);
-    res.send('<script type="text/javascript">alert("가입을 완료하였습니다." + key);document.location.href="/index.html";</script>');
+    res.send('<script type="text/javascript">alert("가입을 완료하였습니다.");document.location.href="/index.html";</script>');
   })
   //module.exports = router;
 
