38d9f23c1bdc0aa7fbf99a52a32eba9f60ff982e
ozyeon <0609ojy@naver.com>
2019-06-11 12:25:17 +0900

    수정

--- HTML/routes/nodemailerTest.js
+++ HTML/routes/nodemailerTest.js
@@ -99,10 +99,10 @@ module.exports = function (app) {
       if (error2) {
         console.log(error2);
       } else{
-        key += data[0].count;
         console.log("key 값은: "+key);
       }
     });
+    key += data[0].count;
     await invoke.invoke(key, inputName, studentNumber, phone, password);
     res.send('<script type="text/javascript">alert("가입을 완료하였습니다. 회원님의 아이디는 '+ key +'");document.location.href="/index.html";</script>');
   })
